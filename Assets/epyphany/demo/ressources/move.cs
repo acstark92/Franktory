849ca14eb02dc6103b5895414325cb5739717d5e
acstark92 <acstark92@gmail.com>
2021-02-10 16:52:26 -0600

    Initial population of repo

---
+++ Assets/epyphany/demo/ressources/move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+	public float speed;
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+	}
+}
---
+++ Assets/epyphany/demo/ressources/rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour
+{
+	public float turnSpeed = 15f;
+	
+	
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+	}
+}
