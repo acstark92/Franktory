5f7852bc10ee34bb3cbc3ac41784598ad7330816
acstark92 <acstark92@gmail.com>
2022-08-19 00:04:31 -0500

    Update to 1.3 - batch 11

---
+++ Assets/Plugins/AnvilRandomPrefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using WurstMod.Any;
+using WurstMod.Shared;
+using Random = System.Random;
+
+public class AnvilRandomPrefab : MonoBehaviour
+{
+	public ResourceDefs.AnvilAsset[] anvilAssetsToLoad;
+
+	private void Awake()
+	{
+		var ldr = gameObject.AddComponent<WurstMod.MappingComponents.Generic.AnvilPrefab>();
+		ldr.prefab = anvilAssetsToLoad[new Random().Next(0, anvilAssetsToLoad.Length)];
+		ldr.Spawn();
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = new Color(0.0f, 0.0f, 0.6f, 0.5f);
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawSphere(Vector3.zero, 0.1f);
+	}
+}
